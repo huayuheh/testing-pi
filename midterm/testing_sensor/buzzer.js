05b611d8035d31200696a50fc96669cef99c30d4
Hana Huayu Heh <anubiss13477@gmail.com>
2016-10-21 00:53:47 -0700

    fix buzzer

--- midterm/testing_sensor/buzzer.js
+++ midterm/testing_sensor/buzzer.js
@@ -1,11 +1,20 @@
 var Gpio = require('onoff').Gpio,
+  motion = new Gpio(21, 'in', 'both');
   buzzer = new Gpio(16, 'out'),
+  buzzerState = 0;
 
+  buzzer.writeSync(0);
 
-  buzzer.writeSync(1);
+  motion.watch( function(err, val){
+  console.log(val);
+  if( err ) { console.log('Motion in 21 Error'); return; }
+  
+  buzzer.writeSync(val);
+});
 
 function exit() {
   buzzer.unexport();
+  motion.unexport();
   process.exit();
 }
  
