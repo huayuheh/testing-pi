5c72b9d14f041a219143398a0311ea41b8165946
hanaheh <hanaheh128@gmail.com>
2016-10-27 23:18:42 -0700

    change typo

--- midterm1/server/public/js/index.js
+++ midterm1/server/public/js/index.js
@@ -10,10 +10,12 @@ var app = angular.module('feeder', [ ]);
       $scope.captureTime = 0;
       $scope.captureImage = "00.jpg";
     socket.on('event:photo', function( photoTime ) {
-      console.log("reseve data "+ photoTime);
-      $scope.captureNote = "Detacted a motion";
-      $scope.captureTime = photoTime;
-      $scope.captureImage = photoTime + ".jpg";
+      console.log("receive data "+ photoTime);
+      $scope.$apply( function() {
+          $scope.captureNote = "Detacted a motion";
+          $scope.captureTime = photoTime;
+          $scope.captureImage = photoTime + ".jpg";
+      });
     });
 
             $scope.buzzer = function(){
