571aef2fa8729526a878100037307ee2f252ecb6
hanaheh <hanaheh128@gmail.com>
2016-10-27 23:24:58 -0700

    Socket io can't recieve signal from website

--- midterm1/server/config/motion.js
+++ midterm1/server/config/motion.js
@@ -20,21 +20,19 @@ motion.watch( function(err, val){
 
   }
 });
- if( io ) {
 
-   io.socket.on('event:buzzer', function () {
-     buzzer.writeSync(1);
-     console.log("turn on buzzer ");
-     setTimeout(function () {
+socket.on('event:buzzer', function () {
+  buzzer.writeSync(1);
+  console.log("turn on buzzer ");
+  setTimeout(function () {
        buzzer.writeSync(0);
      }, 3000);
    });
 
-   io.socket.on('event:video', function () {
+socket.on('event:video', function () {
      console.log("record a video");
 
    });
- }
 
 process.on('SIGINT', function(){
   motion.unexport();
