039aaaa2d15d12ddab0d0db0f6194a469f52a9d0
hanaheh <hanaheh128@gmail.com>
2016-10-27 22:05:24 -0700

    add apply function to change value

--- midterm1/server/public/js/index.js
+++ midterm1/server/public/js/index.js
@@ -6,48 +6,30 @@ socket.on('event:hello', function(){
 
 var app = angular.module('feeder', [ ]);
   app.controller('CamaraController', function($scope){
-      $scope.captureNote = "No motion";
-      $scope.captureTime =  ;
-      $scope.captureImage = "00.jpg";
-    socket.on('event:photo', function( photoTime ) {
-      console.log("reseve data "+ photoTime);
+    $scope.captureNote = "No motion";
+    $scope.captureTime =  ;
+    $scope.captureImage = "00.jpg";
+  socket.on('event:photo', function( photoTime ) {
+    console.log("reseve data "+ photoTime);
+    $scope.$apply( function() {
       $scope.captureNote = "Detacted a motion";
       $scope.captureTime = photoTime;
       $scope.captureImage = photoTime + ".jpg";
     });
+  });
 
-            $scope.buzzer = function(){
-                console.log("Buzzer");
-                socket.emit('event:buzzer', true);
-            };
-            $scope.video = function(){
-                console.log("video");
-                socket.emit('event:video', true);
-            };
-            $scope.identify = function(){
-                console.log("identify");
-            };
-
-
-    });
-
-    var alarm =
-        {
-            date:Date.now(),
-            text: "alert: something close to the feeder",
-            image: 'Case.png'
-        };
+  $scope.buzzer = function(){
+      console.log("Buzzer");
+      socket.emit('event:buzzer', true);
+  };
+  $scope.video = function(){
+      console.log("video");
+      socket.emit('event:video', true);
+  };
+  $scope.identify = function(){
+      console.log("identify");
+  };
+});
 
 
 
-
-// angular.module('mahrio-motion-client', [])
-//   .controller('MainCtrl', ['$scope', function( $scope ) {
-//     $scope.motion = false;
-//     socket.on('event:motion', function( val ) {
-//       console.log('Motion in 21 is ' +(val ? 'ACTIVE' : 'INACTIVE'));
-//       $scope.$apply( function() {
-//         $scope.motion = val ? true : false;
-//       });
-//     });
-//   }]);
