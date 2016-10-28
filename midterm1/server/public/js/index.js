05675865690751a057df729718e472188c16ff70
Hana Huayu Heh <anubiss13477@gmail.com>
2016-10-27 22:22:50 -0700

    fix

--- midterm1/server/public/js/index.js
+++ midterm1/server/public/js/index.js
@@ -6,30 +6,29 @@ socket.on('event:hello', function(){
 
 var app = angular.module('feeder', [ ]);
   app.controller('CamaraController', function($scope){
-    $scope.captureNote = "No motion";
-    $scope.captureTime = 0;
-    $scope.captureImage = "00.jpg";
-  socket.on('event:photo', function( photoTime ) {
-    console.log("reseve data "+ photoTime);
-    $scope.$apply( function() {
+      $scope.captureNote = "No motion";
+      $scope.captureTime = 0;
+      $scope.captureImage = "00.jpg";
+    socket.on('event:photo', function( photoTime ) {
+      console.log("reseve data "+ photoTime);
       $scope.captureNote = "Detacted a motion";
       $scope.captureTime = photoTime;
       $scope.captureImage = photoTime + ".jpg";
     });
-  });
 
-  $scope.buzzer = function(){
-      console.log("Buzzer");
-      socket.emit('event:buzzer', true);
-  };
-  $scope.video = function(){
-      console.log("video");
-      socket.emit('event:video', true);
-  };
-  $scope.identify = function(){
-      console.log("identify");gi
-  };
-});
+            $scope.buzzer = function(){
+                console.log("Buzzer");
+                socket.emit('event:buzzer', true);
+            };
+            $scope.video = function(){
+                console.log("video");
+                socket.emit('event:video', true);
+            };
+            $scope.identify = function(){
+                console.log("identify");
+            };
 
+
+    });
 
 
