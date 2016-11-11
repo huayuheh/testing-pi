2e2696a7244155e6c5060d8faa9a78057182b8ac
huayuheh <anubiss13477@gmail.com>
2016-11-11 14:27:18 -0800

    fix marge problem

--- midterm1/mobile/www/js/controllers.js
+++ midterm1/mobile/www/js/controllers.js
@@ -2,16 +2,12 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
   var showTime = new Date();
-<<<<<<< HEAD
-  $scope.captureNote = "No motion";
-  $scope.captureTime = showTime;
-  $scope.captureImage = "/assets/img/logo-color-s.jpg";
-=======
+
   var severIPAddress = "http://192.168.2.2:8080";
   $scope.captureNote = "No motion";
   $scope.captureTime = showTime;
   $scope.captureImage = severIPAddress + "/assets/img/logo-color-s.jpg";
->>>>>>> a3caff2bdb25990061349dfeab5458c3f2cea3d8
+
   socket.on('event:photo', function( photoTime ) {
     console.log("receive data "+ photoTime);
     setTimeout(function(){
@@ -19,11 +15,9 @@ angular.module('starter.controllers', [])
         $scope.captureNote = "Detacted a motion";
         showTime = new Date(photoTime);
         $scope.captureTime = showTime;
-<<<<<<< HEAD
-        $scope.captureImage ="/assets/img/" + photoTime + ".jpg";
-=======
+
         $scope.captureImage = severIPAddress + "/assets/img/" + photoTime + ".jpg";
->>>>>>> a3caff2bdb25990061349dfeab5458c3f2cea3d8
+
       });
     },500);
   });
