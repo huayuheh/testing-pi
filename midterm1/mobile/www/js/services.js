85cb19b461a575cbd29122bd1ecc60237fe64e11
hanaheh <hanaheh128@gmail.com>
2016-11-27 20:50:19 -0800

    fix mobile views

--- midterm1/mobile/www/js/services.js
+++ midterm1/mobile/www/js/services.js
@@ -6,24 +6,24 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Checkadee',
-    lastText: '2016-11-28 03:42:30',
-    face: 'img/ben.png'
+    name: 'Goldfinch',
+    lastText: '2016-11-28 17:34:58',
+    face: 'img/american_goldfinch.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Cassins Finch',
+    lastText: '2016-11-27 07:28:21',
+    face: 'img/cassins_finch.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Titmouse',
+    lastText: '2016-11-25 07:42:30',
+    face: 'img/black-crested_titmouse.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'Woodpecker',
+    lastText: '2016-11-22 08:13:42',
+    face: 'img/downy_woodpecker.jpg'
   }];
 
   return {
