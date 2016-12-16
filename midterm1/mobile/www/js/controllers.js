434eaa5a6084aefadbe15aa3181d649fa556cc4f
hanaheh <hanaheh128@gmail.com>
2016-12-16 09:55:40 -0800

    upload the presentation vedeos and pdf

--- midterm1/mobile/www/js/controllers.js
+++ midterm1/mobile/www/js/controllers.js
@@ -2,14 +2,14 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
   var showTime = new Date();
-
-  var severIPAddress = "http://localhost:8100";
+  var photoTimeSave = showTime;
+  var severIPAddress = "http://10.0.1.34:8080";
   $scope.captureNote = "No motion";
   $scope.captureTime = "";
   $scope.captureImage = severIPAddress + "/assets/img/logo-color-s.jpg";
 
   socket.on('event:photo', function( photoTime ) {
-    console.log("receive data "+ photoTime);
+    photoTimeSave = photoTime;
     setTimeout(function(){
       $scope.$apply( function() {
         $scope.captureNote = "Detacted a motion";
@@ -25,15 +25,15 @@ angular.module('starter.controllers', [])
     console.log("Buzzer");
     socket.emit('event:buzzer', true);
   };
-  $scope.video = function(){
-    console.log("video");
-    socket.emit('event:video', true);
-  };
-  $scope.identify = function(){
 
-    localStorage.setItem("time", showTime);
-    console.log(localStorage.getItem("time"));
-    document.getElementById("savedtime").innerHTML = localStorage.getItem("lastname");
+  $scope.identify = function(){
+    console.log("save it" + photoTimeSave );
+    $scope.chats = [];
+    $scope.chats.unshift(
+      {id: 4,
+        name: 'checkadee',
+        lastText: '2016-12-08 08:13:42',
+        face: '/assets/img/' + photoTimeSave + '.jpg'});
 
   };
 })
@@ -61,28 +61,12 @@ angular.module('starter.controllers', [])
     };
 })
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  //console.log(chats);
-  $scope.chats = [];
-
-
-
   console.log(localStorage.getItem("time"));
   $scope.chats = Chats.all();
   $scope.reverse = function(chat) {
     Chats.reverse(chat);
   };
-  $scope.chats.unshift(
-    {id: 4,
-      name: 'checkadee',
-      lastText: '2016-12-08 08:13:42',
-      face: '/assets/img/downy_woodpecker.jpg'});
+
 
 
 
